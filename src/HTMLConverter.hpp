bf6a0d3e38d610adcc6e62e1c16a48f89cda0c9f
rlongden15 <rlongden@carthage.edu>
2026-01-21 21:37:16 +0000

    added read in file functionality

---
+++ src/HTMLConverter.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class HTMLConverter{
+    public:
+    HTMLConverter(string inPutFile, string outPutFile);
+    void readInFile(string path);
+
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
-
+#include <iostream>
+#include "HTMLConverter.hpp"
+using namespace std;
 
 int main() {
-    
+    HTMLConverter testCase("/workspaces/ProfMark_Down/tests/MarkdownFile.md", "/workspaces/ProfMark_Down/tests/HTMLFile.html");
     return 0;
 }
